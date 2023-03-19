5c6647cc008fc9f325ee262d3e4235ac9fefc389
JamaalHarris <jamaalmalikharris@gmail.com>
2023-03-19 15:16:45 -0500

    adds create option and route

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,3 +3,11 @@ def index
   @contacts = Contact.all
   render template: "contacts/index" 
 end
+
+
+def create
+  contact = Contact.new(
+    name: params[:name],
+    email: params[:email],
+    phone_number: params[:phone_number]
+  )
